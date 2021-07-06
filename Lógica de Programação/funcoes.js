10a41ba17ca9e446eee61cc0830e66a2ec05d0d8
Renan Arimatea <renanmoraiz@gmail.com>
2021-07-06 07:16:51 -0300

    Trabalhando com funçoês

---
+++ Lógica de Programação/funcoes.js
@@ -0,0 +1,26 @@
+function escreva(texto){
+    console.log(texto)
+}
+
+escreva('Olá, Mundo!')
+
+escreva('Aprendendo Lógica de Programação')
+
+
+
+function somar(a,b){
+    return a + b
+}
+
+let resultado = somar(1,3)
+console.log(resultado)
+
+
+
+const somar1 = function(a,b){return a+b}
+console.log(somar(1,3))
+
+
+
+const somar2 = (a,b) => a+b
+console.log(somar(1,3))
