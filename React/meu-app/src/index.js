a99055b20fea1b020447b20a136119884089cf7a
Renan Arimatea <renanmoraiz@gmail.com>
2021-07-15 23:06:36 -0300

    Trabalhando com as propriedades 'props' e 'estado' no React.

--- React/meu-app/src/index.js
+++ React/meu-app/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import App2 from './components/App2';
+import App3 from './components/App3';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App3>
+      
+    </App3>
   </React.StrictMode>,
   document.getElementById('root')
 );
