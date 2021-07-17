c5c8e2bc4f36cf44001ae4fd90f9977a08bcb482
Renan Arimatea <renanmoraiz@gmail.com>
2021-07-16 21:17:33 -0300

    Adicionando conceitos de renderização condicional e ciclo de vida.

---
+++ React/meu-app/src/components/App4.js
@@ -0,0 +1,40 @@
+import React from 'react'
+class App4 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {nome: undefined, txtName: ""};
+    }
+
+    changetxtName = (event) => {
+        this.setState({txtNome: event.target.value})
+    }
+
+    persistName = () => {
+        this.setState({nome: this.state.txtNome})
+    }
+
+    render() {
+        const renderForm = () => {
+            return (
+                <>
+                Nome: <input type="text" value={this.state.nome} onChange={this.changetxtName}></input>
+                <button onClick={this.persistName}>Salvar</button>
+                </>
+            );
+        }    
+
+        const renderTxt = () => {
+            return ( 
+                <>
+                    <p>
+                        Olá, {this.state.nome}
+                    </p>
+                </>
+            );
+        }
+
+        return !this.state.nome ? renderForm() : renderTxt();
+    }
+}
+
+export default App4;
--- React/meu-app/src/index.js
+++ React/meu-app/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import App2 from './components/App2';
 import App3 from './components/App3';
+import App4 from './components/App4';
+import App5 from './components/App5';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App3>
+    <App5>
       
-    </App3>
+    </App5>
   </React.StrictMode>,
   document.getElementById('root')
 );
